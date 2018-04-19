edfcf3c905e5bb944b678d7d250fb5368ba5a6b1
FatmaUVA <eng.f_06@hotmail.com>
2018-04-19 12:11:10 -0400

    install script

---
+++ install-for-vm.sh
@@ -0,0 +1,16 @@
+
+#install sshd ifconfig vim
+sudo apt-get -y update
+sudo apt-get -y install vim net-tools opens-server tshark
+
+#install x2go
+sudo apt-get -y install software-properties-common python-software-properties
+sudo add-apt-repository ppa:x2go/stable
+sudo apt-get update
+sudo apt-get -y install x2goserver x2goserver-xsession
+
+#install vnc
+#sudo apt-get -y install tightvncserver
+
+#install mplayer
+sudo apt-get install mplayer mplayer-gui mplayer-skin
