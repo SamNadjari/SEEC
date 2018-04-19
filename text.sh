01a042798363fb0215bd802cc374c6867811f136
k <eng.f_06@hotmail.com>
2018-04-19 16:09:53 -0400

    update

---
+++ text.sh
@@ -0,0 +1,3 @@
+
+
+ssh k@192.168.122.216 "sh SEEC/export-display-client.sh run-1"
--- web-page-visit.py
+++ web-page-visit.py
@@ -7,18 +7,26 @@ import time, os
 run_no = 2
 sleep_time = 40
 rdp = "new-teamviewer-with-vm-client-loss-0.1%"
+client_IP = "192.168.122.216"
+client_user = "k"
 
 #for some reasons when I run this command here, it cannot set the variable, so I did it in another shell script which calls this script
 #os.system("export DISPLAY=:0")
 
+file_name = "run-"+str(run_no)+"-python-"+rdp+"-"+str(sleep_time)+"sec-sleep-no-filter"
+
 print("start tcpdump")
-tcpdump_command = "sudo tcpdump -B 4096 -i ens3 -w run-"+str(run_no)+"-python-"+rdp+"-"+str(sleep_time)+"sec-sleep-no-filter-mv.pcap &"
+tcpdump_command = "sudo tcpdump -B 4096 -i ens3 -w "+file_name+".pcap &"
 os.system(tcpdump_command)
 
 time.sleep(sleep_time)
 
-print("Launch firefox")
+#capture display at the client
+print("Capture client display")
+capture_display_commnad = "ssh "+client_user+"@"+client_IP+" \"sh SEEC/export-display-client.sh "+file_name+"-1\""
+os.system(capture_display_commnad)
 
+print("Launch firefox")
 #measure firefox launch time
 #os.system("DISPLAY=:0 firefox &") #setup DISPLAY to the teamviewer display to launch firefox there
 os.system("firefox &")
@@ -34,10 +42,20 @@ webbrowser.get('firefox').open('https://pinchofyum.com')
 print("sleep for 10 sec")
 time.sleep(sleep_time)
 
+#capture display at the client
+print("Capture client display")
+capture_display_commnad = "ssh "+client_user+"@"+client_IP+" \"sh SEEC/export-display-client.sh "+file_name+"-2\""
+os.system(capture_display_commnad)
+
 print("open pure image")
 #os.system("DISPLAY=:0 firefox -new-tab -url https://upload.wikimedia.org/wikipedia/commons/1/16/Appearance_of_sky_for_weather_forecast%2C_Dhaka%2C_Bangladesh.JPG")
 webbrowser.get('firefox').open('https://upload.wikimedia.org/wikipedia/commons/1/16/Appearance_of_sky_for_weather_forecast%2C_Dhaka%2C_Bangladesh.JPG')
 time.sleep(sleep_time)
+
+#capture display at the client
+print("Capture client display")
+capture_display_commnad = "ssh "+client_user+"@"+client_IP+" \"sh SEEC/export-display-client.sh "+file_name+"-3\""
+os.system(capture_display_commnad)
 
 print("Kill tcpdump")
 os.system("sudo killall tcpdump")
