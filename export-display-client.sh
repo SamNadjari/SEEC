c809e58dfe280809766edf780a63917f7a59b930
FatmaUVA <eng.f_06@hotmail.com>
2018-04-20 10:23:23 -0400

    fix script

--- export-display-client.sh
+++ export-display-client.sh
@@ -2,4 +2,8 @@
 name=$1
 export DISPLAY=:0
 
-import -window root $name.png
+#screenshot the display
+import -window root SEEC/results/images/$name.png
+#crop the image to remove the time which will make comparing two images quality inaccurate
+#+10+80 represents the X and Y from the top left where you  want to start cropping
+convert SEEC/results/images/$name.png  -crop 1024x600+10+80 SEEC/results/images/$name-cropped.png
