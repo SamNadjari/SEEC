7f217e4be0c26743ee5e8cf19fcbf81db2f47ae0
harlem1 <harlem1@router.asus>
2018-09-17 16:49:34 -0400

    thru script

--- Windows-scripts/compute-thru.sh
+++ Windows-scripts/compute-thru.sh
@@ -1,4 +1,4 @@
 
 pcap_file=$1
 client_IP=$2
-tshark -q -nr $pcap_file -z io,stat,0.001,ip.dst==$client_IP,"ip.dst==$client_IP && udp.port==60000" | grep "<>" | awk '{print $8 " " $12}'
+tshark -q -nr $pcap_file -z io,stat,0.001,ip.dst==$client_IP,"ip.dst==$client_IP && udp.port==60000" | grep "<>" | awk '{print $8 " " $12}' > thru_data.txt
