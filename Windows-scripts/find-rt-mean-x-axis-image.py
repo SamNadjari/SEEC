d56f57a3c2248086179998740a60a7360f7cca13
FatmaUVA <eng.f_06@hotmail.com>
2018-10-10 11:03:59 -0400

    add plot script

--- Windows-scripts/find-rt-mean-x-axis-image.py
+++ Windows-scripts/find-rt-mean-x-axis-image.py
@@ -1,6 +1,5 @@
 #This is a python3 code
-#input: dst_IP pcap rtt loss app run_no
-#python3 compute-rt-from-display-updates.py 172.28.30.9 capture-1-slow.pcap 200 10 imgeView 13
+#plot where the x axis is the image unique pixels
 
 import sys, os
 import numpy as np
@@ -14,11 +13,15 @@ rtt=[0,20,50,100,200]
 loss=[0,3,5]
 app="ImageView"
 total_runs=10
-res_dir="/home/harlem1/SEEC/Windows-scripts/results"
-plot_dir='/home/harlem1/SEEC/Windows-scripts/plots/new-mean'
 method=["display_updates_2"] #["autoit","display_updates","display_updates_2"] #"RT_marker_packets_2"
 run_no=16
 no_tasks=6
+pixels_count = [18675,24639,129190,309237,563443,733950] #no of unique pixels in each image
+
+res_dir="/home/harlem1/SEEC/Windows-scripts/results"
+plot_dir='/home/harlem1/SEEC/Windows-scripts/plots/new-mean'
+plot_name='/mean-RT-image-x-axis-'+app+'-total-runs-'+str(total_runs)+'-run-'+str(run_no)+'.png'
+
 #===================Read data======================
 
 for meth in method:
@@ -45,20 +48,26 @@ print("unique loss values = ",loss_uniq)
 rtt_unique = np.unique(rtt)
 print("unique rtt values = ",rtt_unique)
 
-#find indices where loss value equal to specific value
+
+#find indices where loss value equal to specific value and RTT of 0
+rtt_0 = np.where(rtt==0)
+print("rtt_0 ", rtt_0)
 for l in loss_uniq:
     temp1 = "loss_" + str(l) 
-    globals()[temp1] = np.where(loss==l)
+    x = np.where(loss==l)
+    globals()[temp1] = np.intersect1d(x,rtt_0)
     #print("indices, " ,temp1,"  = ",globals()[temp1]) 
     #convert it to a list structure to easily access elemts in a loop
     temp2 = "loss_" + str(l) + "_index"
     globals()[temp2] = []
 
-    #add elements to the new list
-    for i in range(len(globals()[temp1][0])):
-        globals()[temp2].append(globals()[temp1][0][i])
 
-#create arrays for each image based on loss value
+
+    #add elements to the new list
+    for i in range(len(globals()[temp1])):
+        globals()[temp2].append(globals()[temp1][i])
+
+#create arrays for each 
 for meth in method:
     for i in range(no_tasks):
         for l in loss_uniq:
@@ -72,85 +81,67 @@ for meth in method:
                 globals()[temp2] = []
 
 #append values to the arrays based on loss values
+#each image will hav xx (based on the total_runs) value for each loss rate
 for meth in method:
-    for i in range(no_tasks):
-        for l in loss_uniq:
+    for l in loss_uniq:
+        #create arrays to hold the mean value of rt for each image based on loss value (each array withh have the mean of each image as one row)
+        temp5 = "rt_"+meth+"_loss_" + str(l) + "_mean"
+        temp6 = "by_"+meth+"_loss_" + str(l) + "_mean"
+        globals()[temp5] = []
+        globals()[temp6] = []
+        for i in range(no_tasks):
             rttx = []
             index_array = "loss_" + str(l) + "_index"
+            temp2 = "rt_"+meth+"_"+str(i)
+            temp1 = "rt_"+meth+"_"+str(i)+"_loss_" + str(l)
+            temp3 = "by_"+meth+"_"+str(i)+"_loss_" + str(l)
             for j in range(len(globals()[index_array])):
-                index = globals()[index_array][j]
-                temp2 = "rt_"+meth+"_"+str(i)
-                temp1 = "rt_"+meth+"_"+str(i)+"_loss_" + str(l)
-                temp3 = "by_"+meth+"_"+str(i)+"_loss_" + str(l)
-                rttx.append(rtt[index])
-                
+                index = globals()[index_array][j]                
                 globals()[temp1].append(globals()[temp2][index])
                 if meth != "autoit": #it doesn't have total no of bytes to read
                     temp4 = "by_"+meth+"_"+str(i)
                     globals()[temp3].append(globals()[temp4][index])
-            #reshape array to find mean 
-            temp1 = "rt_"+meth+"_"+str(i)+"_loss_" + str(l)
-            temp3 = "by_"+meth+"_"+str(i)+"_loss_" + str(l)
-            globals()[temp1] = np.asarray(globals()[temp1])
-            globals()[temp1] = np.reshape(globals()[temp1],(total_runs,len(rtt_unique)))
-            if meth != "autoit":
-                globals()[temp3] = np.asarray(globals()[temp3])
-                globals()[temp3] = np.reshape(globals()[temp3],(total_runs,len(rtt_unique)))
-
-#temp1 = "rt_"+method[0]+"_"+str(i)+"_loss_" + str(l)
-#print(temp1, " ",globals()[temp1])
-#print("len ",temp1 , len(globals()[temp1]))
-
-# find the mean of the xx total runs
-for meth in method:
-    for i in range(no_tasks):
-        for l in loss_uniq:
-            temp1 = "rt_"+meth+"_"+str(i)+"_loss_" + str(l)
-            temp3 = "by_"+meth+"_"+str(i)+"_loss_" + str(l)
-            temp2 = "rt_"+meth+"_"+str(i)+"_loss_" + str(l) + "_mean"
-            temp4 = "by_"+meth+"_"+str(i)+"_loss_" + str(l) + "_mean"
-
-            globals()[temp2] = np.mean(globals()[temp1], axis=0) #take the mean across column, which means across each rtt value
-            if meth != "autoit":
-                globals()[temp4] = np.mean(globals()[temp3], axis=0)
-                #print(temp4," ", globals()[temp4]) 
-
+        #print("temp1, ",temp1," ",globals()[temp1])
+            #find the mean and add it to an array that have all images in one row
+            globals()[temp5].append(np.mean(globals()[temp1]))
+            if meth != "autoit": 
+                globals()[temp6].append(np.mean(globals()[temp3]))
+            #print("temp5, ",temp5," ",globals()[temp5])
 
 #=================================Plot======================================
 
-rtt = np.asarray(rtt_unique)
+#pixels_count = [] #no of unique pixels in each image
 colors = cm.rainbow(np.linspace(0, 7, 20))
-markers = ['^','s','o','*','x','D','+'] 
-for i in range(no_tasks):
-    fig, ax1 = plt.subplots(1)
-    ax1.set_xlabel('RTT (ms)',fontsize=14)
-    ax1.set_ylabel(app+' load time (sec)')
-    for meth in method:
-        col_index = 0 #index to assign differnt colors for lines
-        for l in loss_uniq:
-            temp2 = "rt_"+meth+"_"+str(i)+"_loss_" + str(l) + "_mean"
+markers = ['^','s','o','*','x','D','+']
+fig, ax1 = plt.subplots(1)
+ax1.set_xlabel('Number of unique pixels',fontsize=14)
+ax1.set_ylabel(app+' load time (sec)')
 
-            ax1.plot(rtt,globals()[temp2],color=colors[col_index],marker=markers[col_index],linewidth=2.0,markersize=10,label = meth+', loss = '+str(l)+"%")
-            col_index = col_index + 1
+for meth in method:
+    col_index = 0 #index to assign differnt colors for lines
+    for l in loss_uniq:
+        temp5 = "rt_"+meth+"_loss_" + str(l) + "_mean"
+        ax1.plot(pixels_count,globals()[temp5],color=colors[col_index],marker=markers[col_index],linewidth=2.0,markersize=10,label = meth+', loss = '+str(l)+"%")
+        col_index = col_index + 1
 
-    #create anothor axis for number of bytes
-    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-    ax2.set_ylabel('Display Update Size (Bytes)')  # we already handled the x-label with ax1
-    for meth in method:
-        col_index = 0 #index to assign differnt colors for lines
-        for l in loss_uniq:
-            if meth != "autoit":
-                temp4 = "by_"+meth+"_"+str(i)+"_loss_" + str(l) + "_mean"
-                ax2.plot(rtt,globals()[temp4],color=colors[col_index],marker=markers[col_index],linestyle='dashed',linewidth=2.0,markersize=10)
+#create anothor axis for number of bytes
+ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+ax2.set_ylabel('Display Update Size (Bytes)')  # we already handled the x-label with ax1
+for meth in method:
+    col_index = 0 #index to assign differnt colors for lines
+    for l in loss_uniq:
+        if meth != "autoit":
+            temp6 = "by_"+meth+"_loss_" + str(l) + "_mean"
+            ax2.plot(pixels_count,globals()[temp6],color=colors[col_index],marker=markers[col_index],linestyle='dashed',linewidth=2.0,markersize=10,label = meth+',bytes, loss = '+str(l)+"%")
 
-            col_index = col_index + 1
+        col_index = col_index + 1
 
-    ax1.legend(loc='upper left',ncol=3,bbox_to_anchor=(0,1.18))
+ax1.legend(loc='upper left',ncol=3,bbox_to_anchor=(-0.5,1.18))
+ax2.legend(loc='upper left',ncol=3,bbox_to_anchor=(-0.5,-0.1))
     #save the plot for each image
-    plt.savefig(plot_dir + '/mean-RT-'+app+'-image-'+str(i)+'-run-'+str(run_no)+'.png',format="png",bbox_inches='tight')
-    plt.close()
-        
-'''
+plt.savefig(plot_dir + '/' +plot_name,format="png",bbox_inches='tight')
+#plt.close()
+'''        
 #===============================Save Results=========================
 #save results to file
 #change to np array to save file
